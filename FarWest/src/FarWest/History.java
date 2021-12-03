ceb4610b0f47d8f8b303c35a31952d4a7a148d33
augustinguillon <aguillon.ent@gmail.com>
2021-12-03 12:29:44 +0100

    Augustin

--- FarWest/src/FarWest/History.java
+++ FarWest/src/FarWest/History.java
@@ -3,6 +3,7 @@ package FarWest;
 public class History {
 
     public static void main(String[] args) {
-
+        Human human1 = new Human("Boris", "neige fondu goût rivella");
     }
+
 }
--- FarWest/src/FarWest/Human.java
+++ FarWest/src/FarWest/Human.java
@@ -2,11 +2,23 @@ package FarWest;
 
 public class Human {
 
-    String name;
-    String boisson = "eau";
+    private String name;
+    private String boisson = "eau";
 
     Human(String name, String boisson){
         this.name = name;
         this.boisson = boisson;
+    }
+
+    private void talk(String text){
+        System.out.println(this.name + " "+text);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBoisson() {
+        return boisson;
     }
 }
