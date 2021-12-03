6b3f228305b0fd4456cd608bd41aec39765b347d
Todd Raharivelo <t.raharivelo@outlook.fr>
2021-12-03 12:43:50 +0100

    feat: done Brigand instanciation

--- java-town/src/Story.java
+++ java-town/src/Story.java
@@ -1,16 +1,51 @@
+import fr.western.models.Brigand;
 import fr.western.models.Human;
 import fr.western.models.Sieur;
 
+import java.util.Scanner;
+
 public class Story {
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Welcome in Reverso Mundo >:D");
-        Human boris = new Human("Boris");
 
+        System.out.println("\nQue veux-tu tester ? (human/sieur/brigand/cowgirl/all) :");
+        String answer = scanner.next().toLowerCase();
+
+        switch(answer) {
+            case "human":
+                testHuman();
+                break;
+            case "sieur":
+                testSieur();
+                break;
+            case "brigand":
+                testBrigand();
+                break;
+            case "cowgirl":
+                testCowgirl();
+                break;
+            default:
+            case "all":
+                testHuman();
+                testSieur();
+                testBrigand();
+                testCowgirl();
+                break;
+        }
+    }
+
+    private static void testHuman() {
+        System.out.println("\n");
+        Human boris = new Human("Boris");
         boris.talk("coucou");
         boris.introduceSelf();
         boris.drink();
+    }
 
+    private static void testSieur() {
+        System.out.println("\n");
         Sieur luke = new Sieur("Lucky Luke");
         Sieur rosario = new Sieur("Rosario DelMundo", "rose");
         Sieur tom = new Sieur ("Tom Sawyer", false);
@@ -21,5 +56,23 @@ public class Story {
         luke.setDressColor("verte");
 
         rosario.getKidnapped();
+    }
+
+    private static void testBrigand() {
+        System.out.println("\n");
+        Brigand joe = new Brigand("Joe Dalton");
+        Brigand william = new Brigand("William Dalton", "cool" );
+        Brigand jack = new Brigand("Jack Dalton", 300);
+        Brigand averell = new Brigand("Averell Dalton", "gentil", 50, true);
+
+        Brigand miaouss = new Brigand("Miaouss Team Rocket", true);
+
+        System.out.println(joe + "\n" + william + "\n" + jack + "\n" + averell + "\n" + miaouss);
+
+
+    }
+
+    private static void testCowgirl() {
+        System.out.println("\n");
     }
 }
--- java-town/src/fr/western/models/Brigand.java
+++ java-town/src/fr/western/models/Brigand.java
@@ -1,8 +1,51 @@
 package fr.western.models;
 
 public class Brigand extends Human {
+    private String look;
+    private int reward;
+    private boolean isInJail;
 
     public Brigand(String name) {
+        this(name, "méchant", 100, false);
+    }
+
+    public Brigand(String name, String look) {
+        this(name, look, 100, false);
+    }
+
+    public Brigand(String name, int reward) {
+        this(name, "méchant", reward, false);
+    }
+
+    public Brigand(String name, boolean isInJail) {
+        this(name, "méchant", 100, isInJail);
+    }
+
+    public Brigand(String name, String look, int reward) {
+        this(name, look, reward, false);
+    }
+
+    public Brigand(String name, String look, boolean isInJail) {
+        this(name, look, 100, isInJail);
+    }
+
+    public Brigand(String name, int reward, boolean isInJail) {
+        this(name, "méchant", reward, isInJail);
+    }
+
+    public Brigand(String name, String look, int reward, boolean isInJail) {
         super(name);
+        this.look = look;
+        this.reward = reward;
+        this.isInJail = isInJail;
+    }
+
+    @Override
+    public String toString() {
+        String captiveState = isInJail ? "en taule, ha" : "en cavale, peuh";
+        return "[ Voici le Brigand " + this.name +
+                ", qui a un look " + look + ". " +
+                reward + " $ pour sa tête... "+
+                "Cette canaille est " + captiveState + " ! ]" ;
     }
 }
